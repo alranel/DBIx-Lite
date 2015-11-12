78be4cf65e02f593e3ffb471229f3f5a205a435e
Alessandro Ranellucci <aar@cpan.org>
2015-11-12 10:22:25 +0100

    Don't supply column_alias to SQL::Abstract::More

--- lib/DBIx/Lite.pm
+++ lib/DBIx/Lite.pm
@@ -19,7 +19,6 @@ sub new {
     my $self = {
         schema      => delete $params{schema} || DBIx::Lite::Schema->new,
         abstract    => SQL::Abstract::More->new(
-            column_alias => '%s AS %s',
             %{ delete $params{abstract} || {} },
         ),
         connector   => delete $params{connector},
