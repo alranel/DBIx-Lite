0396b830b10f1a08edb78aa29c1e7b8ee926024e
Diab Jerius <djerius@cfa.harvard.edu>
2025-07-08 09:05:05 -0400

    create a column alias for COUNT(*) so that optional SQL::Abstract::More column quoting works
    The existing code passes the expression COUNT(*) to SQL::Abstract::More, which
    intepreted it as a column name and if its quote_char option was set, quoted it,
    leading to errors such as
    
    DBD::Pg::st execute failed: ERROR:  column "COUNT(*)" does not exist
    LINE 1: SELECT "COUNT(*)" FROM "temp0002" AS "me"
                    ^ [for Statement "SELECT "COUNT(*)" FROM "temp0002" AS "me""]
    
    This commit creates a column alias for COUNT(*), providing something appropriate for
    SQL::Abstract::More to quote.

--- lib/DBIx/Lite/ResultSet.pm
+++ lib/DBIx/Lite/ResultSet.pm
@@ -541,7 +541,9 @@ sub count {
     my $count;
     $self->{dbix_lite}->dbh_do(sub {
         # Postgres throws an error when using ORDER BY clauses with COUNT(*)
-        my $count_rs = $self->select(\ "COUNT(*)")->order_by(undef);
+        # create a column alias so that if SQL::Abstract::More quotes columns
+        # it'll quote the column "count" rather than the expression "COUNT(*)"
+        my $count_rs = $self->select( [\'COUNT(*)', 'count' ])->order_by(undef);
         my ($sth, @bind) = $count_rs->select_sth;
         $sth->execute(@bind);
         $count = +($sth->fetchrow_array)[0];
